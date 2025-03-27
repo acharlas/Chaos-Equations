e6fcfbbb2091511d108c2f36a2c2e8ef96ae850d
acharlas <acharlas@student.42.fr>
2025-03-27 14:23:34 +0100

    changed the Leva collapsed value depending of the screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Scene from "./Scene";
 import "./App.css";
+import { Leva } from "leva";
 
 export default function App() {
+  //Should be change to a starting value and then change but somehow Leva don't re-render on resize.
+  const [isMobileOrTablet, setIsMobileOrTablet] = useState(
+    window.innerWidth < 1024
+  );
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobileOrTablet(window.innerWidth < 1024);
+    };
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <div className="App">
+      <Leva collapsed={isMobileOrTablet} />
       <Scene />
     </div>
   );
