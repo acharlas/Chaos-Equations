bc148a1813e1dc29c6ba7c8ec4dd2b0e8151c099
acharlas <acharlas@student.42.fr>
2026-02-04 20:08:33 +0100

    Wrap app with tooltip provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Scene from "./Scene";
 import "./App.css";
 import { Leva } from "leva";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 export default function App() {
   //Should be change to a starting value and then change but somehow Leva don't re-render on resize.
@@ -19,15 +20,17 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div className="leva-panel">
-        <Leva
-          fill
-          collapsed={isMobileOrTablet}
-          titleBar={{ title: "Settings", drag: false }}
-        />
+    <TooltipProvider>
+      <div className="App">
+        <div className="leva-panel">
+          <Leva
+            fill
+            collapsed={isMobileOrTablet}
+            titleBar={{ title: "Settings", drag: false }}
+          />
+        </div>
+        <Scene />
       </div>
-      <Scene />
-    </div>
+    </TooltipProvider>
   );
 }
