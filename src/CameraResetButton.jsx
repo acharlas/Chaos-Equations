4f45526d991270622d9219c0dd2e7e96b6ab6a0a
acharlas <acharlas@student.42.fr>
2026-05-02 12:10:46 +0200

    chore: remove console.log from CameraResetButton

--- src/CameraResetButton.jsx
+++ src/CameraResetButton.jsx
@@ -14,7 +14,6 @@ const CameraResetButton = ({ controlsRef }) => {
           camera.updateProjectionMatrix();
         }
         camera.lookAt(0, 0, 0);
-        console.log("Camera reset to:", camera.position);
         if (controlsRef && controlsRef.current && controlsRef.current.reset) {
           controlsRef.current.reset();
         }
